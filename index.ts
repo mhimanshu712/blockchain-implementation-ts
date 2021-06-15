58317a6900d28f5bdf2d23c3676a72a650f5fd3c
Himanshu Mishra <mhimanshu712@gmail.com>
2021-06-15 13:56:15 +0530

    Wallet and Chain added

--- index.ts
+++ index.ts
@@ -27,3 +27,40 @@ class Block {
         return hash.digest('hex');
     }
 }
+
+class Chain {
+    public static instance = new Chain();
+
+    chain: Block[];
+
+    constructor() {
+        this.chain = [new Block(null, new Transaction(100, 'genesis','him'))];
+    }
+
+    get lastBlock() {
+        return this.chain[this.chain.length -1];
+    }
+
+    addBlock(transaction: Transaction, senderPublicKey: string, signature: string){
+        const newBlock = new Block(this.lastBlock.hash, transaction);
+        this.chain.push(newBlock);
+    }
+}
+
+class Wallet {
+    public publickey: string;
+    public privatekey: string;
+
+    constructor() {
+        const keypair = crypto.generateKeyPairSync('rsa', {
+            modulusLength: 2048,
+            publicKeyEncoding: {type: 'spki', format: 'pem'},
+            privateKeyEncoding: { type: 'pkcs8', format: 'pem'},
+        });
+
+        this.privatekey = keypair.privateKey;
+        this.publickey = keypair.publicKey;
+    }
+
+    
+}
