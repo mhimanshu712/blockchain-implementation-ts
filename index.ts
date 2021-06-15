d29e3378a124b0d954ab230106bbf8db3fa28aa6
Himanshu Mishra <mhimanshu712@gmail.com>
2021-06-15 13:41:32 +0530

    Hashing added

--- index.ts
+++ index.ts
@@ -19,4 +19,11 @@ class Block {
        public transaction: Transaction,
        public ts = Date.now()
     ){}
+
+    get hash() {
+        const str = JSON.stringify(this);
+        const hash = crypto.createHash('SHA256');
+        hash.update(str).end();
+        return hash.digest('hex');
+    }
 }
